b77524bb472b4d0ffd8d20db254f5f8f3729aad2
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-07-23 09:42:22 +0530

    Implement blog post editing functionality and enhance delete action with form submission

--- index.js
+++ index.js
@@ -19,13 +19,32 @@ app.post("/add_blog", (req, res) => {
     let title = req.body["title"];
     let content = req.body["content"];
     posts.push({ title: title, content: content });
-    res.render("index.ejs", {
-        title: title,
-        content: content,
-        posts: posts
+    res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    posts.splice(posts.length - 1 - id, 1);
+    res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const post = posts[posts.length - 1 - id];
+    res.render("edit.ejs", {
+        post: post,
+        id: id
     });
 });
 
+app.post("/edit/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    posts[posts.length - 1 - id] = {
+        title: req.body["title"],
+        content: req.body["content"]
+    };
+    res.redirect("/");
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
