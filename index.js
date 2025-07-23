da64fdb8d52cde940f91c2b76b246f9fbd286072
Pawan Thathsara <pawanthathsara987@gmail.com>
2025-07-23 08:19:14 +0530

    Refactor blog post rendering in index.ejs to display posts dynamically and improve code structure

--- index.js
+++ index.js
@@ -7,17 +7,22 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/",(req,res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {
+        posts: posts
+    });
 });
 
 app.post("/add_blog", (req, res) => {
     let title = req.body["title"];
     let content = req.body["content"];
-    
+    posts.push({ title: title, content: content });
     res.render("index.ejs", {
         title: title,
-        content: content
+        content: content,
+        posts: posts
     });
 });
 
